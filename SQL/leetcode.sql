888becc7728a7122c13bb2ebf76e622d0ec32636
Olena Ilina <lenailinav@gmail.com>
2024-08-08 15:17:56 +0200

    Add new solutions

--- SQL/leetcode.sql
+++ SQL/leetcode.sql
@@ -162,3 +162,89 @@ LEFT JOIN UnitsSold u ON p.product_id = u.product_id
 AND u.purchase_date BETWEEN p.start_date AND p.end_date
 GROUP BY p.product_id
 ;
+
+-- 1075. Project Employees I
+-- Write an SQL query that reports the average experience years of all the employees for each project, rounded to 2 digits.
+SELECT
+    p.project_id
+    , ROUND(AVG(e.experience_years),2) AS average_years
+FROM Project p
+LEFT JOIN Employee e ON p.employee_id = e.employee_id
+GROUP BY p.project_id
+;
+
+-- 1211. Queries Quality and Percentage
+-- We define query quality as:
+--The average of the ratio between query rating and its position.
+--We also define poor query percentage as:
+--The percentage of all queries with rating less than 3.
+-- Write a solution to find each query_name, the quality and poor_query_percentage.
+--Both quality and poor_query_percentage should be rounded to 2 decimal places.
+SELECT
+    query_name
+    , ROUND(AVG(rating/position), 2) AS quality
+    , ROUND(SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END) * 100 / COUNT(*),2) AS poor_query_percentage
+FROM Queries
+WHERE query_name IS NOT NULL
+GROUP BY query_name
+;
+
+-- 1193. Monthly Transactions I
+-- Write an SQL query to find for each month and country, the number of transactions and their total amount, the number of approved transactions and their total amount.
+SELECT
+    DATE_FORMAT(trans_date, '%Y-%m') AS month
+    , country
+    , COUNT(*) AS trans_count
+    , COUNT(CASE WHEN state = 'approved' THEN 1 ELSE NULL END) AS approved_count
+    , SUM(amount) AS trans_total_amount
+    , SUM(CASE WHEN state = 'approved' THEN amount ELSE 0 END) AS approved_total_amount
+FROM Transactions
+GROUP BY DATE_FORMAT(trans_date, '%Y-%m'), country
+;
+
+-- 1174. Immediate Food Delivery II
+-- If the customer's preferred delivery date is the same as the order date, then the order is called immediate; otherwise, it is called scheduled.
+-- The first order of a customer is the order with the earliest order date that the customer made. It is guaranteed that a customer has precisely one first order.
+--Write a solution to find the percentage of immediate orders in the first orders of all customers, rounded to 2 decimal places.
+WITH CTE AS (
+    SELECT 
+        customer_id
+        , order_date
+        , customer_pref_delivery_date
+        , ROW_NUMBER() OVER (PARTITION BY customer_id ORDER BY order_date) AS rn
+    FROM Delivery
+)
+
+SELECT
+    ROUND(AVG(CASE WHEN order_date = customer_pref_delivery_date THEN 1 ELSE 0 END) * 100,2) AS immediate_percentage
+FROM CTE
+WHERE rn = 1
+;
+
+-- 550. Game Play Analysis IV
+-- Write a solution to report the fraction of players that logged in again on the day after the day they first logged in, rounded to 2 decimal places. 
+-- In other words, you need to count the number of players that logged in for at least two consecutive days starting from their first login date, then divide that number by the total number of players.
+WITH FirstLogin AS (
+    SELECT
+        player_id
+        , MIN(event_date) AS min_event_date
+    FROM Activity
+    GROUP BY player_id
+),
+
+NewTable AS (
+    SELECT
+        a.player_id
+        , a.event_date
+        -- , f.player_id
+        , f.min_event_date
+        , ROW_NUMBER() OVER (PARTITION BY a.player_id ORDER BY a.event_date) AS rn
+        , DATEDIFF(event_date, min_event_date) as dif_date
+    FROM Activity a
+    LEFT JOIN FirstLogin f ON f.player_id = a.player_id
+)
+
+SELECT
+    ROUND(COUNT(DISTINCT CASE WHEN dif_date = 1 THEN player_id END) * 1.0 / COUNT(DISTINCT player_id), 2) AS fraction
+FROM NewTable;
+;
